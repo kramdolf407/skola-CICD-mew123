db87df907113f914ae86526a519ae989d211974b
Peter Flodmark <pfpeterflodmark@gmail.com>
2018-11-12 10:39:06 +0100

    fixat unittest på check()

---
+++ src/OnlineTestTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OnlineTestTest {
+
+    @org.junit.jupiter.api.Test
+    void check() {
+        OnlineTest onlineTest = new OnlineTest("Test of java");
+        onlineTest.current = 0;
+        onlineTest.jb[1].setSelected(true);
+        assertTrue(onlineTest.check());
+        onlineTest.jb[2].setSelected(true);
+        assertFalse(onlineTest.check());
+        onlineTest.current = 1;
+        onlineTest.jb[2].setSelected(true);
+        assertTrue(onlineTest.check());
+        onlineTest.jb[4].setSelected(true);
+        assertFalse(onlineTest.check());
+
+    }
+}
