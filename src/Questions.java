5049801f401cf552994cbec90845dd1fb95d29cb
usk1129 <bayr_usukh@yahoo.com>
2018-11-22 09:55:17 +0100

    lagt till frågor

--- src/Questions.java
+++ src/Questions.java
@@ -6,8 +6,8 @@ public class Questions {
     String[] q4 = {"String class is defined in which package?", "lang", "Swing", "Applet", "awt"};
     String[] q5 = {"Which institute is best for java coaching?", "Utek", "Aptech", "SSS IT", "jtek"};
     String[] q6 = {"Which one among these is not a keyword?", "class", "int", "get", "if"};
-
-
+    String[] q7 = {"Which one represents a single line comment in Java","//comment","//*comment","@comment","#comment"};
+    String[] q8 = {"Which one represents a multi line comment in Java","//comment","/*comment*/","@comment","#comment"};
     public String[] returnQuestion(int question_id) {
         String[] noValue = new String[5];
         if (question_id == 0) {
@@ -22,7 +22,13 @@ public class Questions {
             return q5;
         } else if (question_id == 5) {
             return q6;
+        } else if (question_id == 6){
+            return q7;
+        } else if (question_id == 7){
+            return q8;
+
         }
+
 
         else return noValue;
 
